49c71485acea5d0ac9c4c488b71e829647d0571c
Mubin Iftikhar <mubin4739@gmail.com>
2023-02-02 11:13:48 +0600

    adding sum

--- main.php
+++ main.php
@@ -1,3 +1,6 @@
 <?php
-echo" Hello this is mubin";
-echo"My name is mubin iftikhar";
+echo" Hello this is mubin\n";
+echo"My name is mubin iftikhar\n";
+$m="Mubin Iftikhar\n";
+$n=10;$v=30;
+echo$n+$v;
